9e9e003c2b895c5fe53c1732f7dea25a4444d012
YaGaskoin <32508888+YaGaskoin@users.noreply.github.com>
2020-03-31 14:07:30 +0300

    registration extends profile and edit profile

--- socio/account/urls.py
+++ socio/account/urls.py
@@ -13,5 +13,7 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('register/', views.register, name="register"),
+    path('edit/', views.edit, name="edit"),
 
 ]
