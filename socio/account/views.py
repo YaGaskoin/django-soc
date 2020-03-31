0016c03ae3a2c5ca4749c85b368701c45020cf1d
YaGaskoin <32508888+YaGaskoin@users.noreply.github.com>
2020-03-31 12:52:56 +0300

    add registration change and reset password

--- socio/account/views.py
+++ socio/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import  login_required
+from django.contrib.auth.decorators import login_required
 from .forms import LoginForm
 
 # Create your views here.
@@ -27,5 +27,6 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
+@login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
